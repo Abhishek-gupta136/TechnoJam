861f6647190818886d40d08ba74e939ebb5e4f34
Abhishek-gupta136 <141557088+Abhishek-gupta136@users.noreply.github.com>
2024-10-22 20:45:32 +0530

    question 2.py

--- question 2.py
+++ question 2.py
@@ -1,18 +1,19 @@
-def generate_pascal_triangle(n):
-    #write your code here
-    ans = [[1],[1,1]]
-    for i in range(3,n+1):
-        let = [1,]
-        work = ans[-1]
-        for j in range(len(work) - 1):
-            s = work[j]+ work[j + 1]
-            let.append(s)
-            
-        let.append(1)
-        ans.append(let)
+def climb_stairs(n):
+  #write your code here
+    if( n == 0 or n == 1) :
+        print(1)
+    
+    
+    step1 = 1
+    step2 = 1
+    current = 0
+    for i in range(2,n + 1):
+        current = step1 + step2
+        step1 = step2
+        step2 = current
         
-    print(ans)
+    return current
 
 if __name__ == "__main__":
-    n = 5
-    generate_pascal_triangle(n)
+    n = 2
+    print(climb_stairs(n))
